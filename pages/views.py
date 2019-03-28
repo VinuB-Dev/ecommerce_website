2b6fc972d48defa4a66033730adf3a924cc06916
vignesh1999 <vinu6th@gmail.com>
2019-03-28 18:12:28 +0530

    prodct description done

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from products.models import Product
+from sellers.models import Seller
+
 def index(request):
-    return render(request,'pages/index.html')
+    products = Product.objects.all().filter(is_published=True)[:3]
+
+    context = {
+        'products': products
+    }
+
+    return render(request,'pages/index.html', context)
 
 
 def about(request):
-    return render(request,'pages/about.html')
+    # Get all sellers
+    sellers = Seller.objects.all()
+
+    context = {
+        'sellers': sellers
+    }
+    return render(request,'pages/about.html', context)
 
--- products/views.py
+++ products/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Product
 
 def index(request):
-    products = Product.objects.all()
+    products = Product.objects.all().filter(is_published=True)
+
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
 
     context = {
-        'products': products
+        'products': paged_products
     }
     return render(request,'products/products.html', context)
 
 def product(request, product_id):
-    return render(request,'products/product.html')
+    product = get_object_or_404(Product, pk=product_id)
+
+    context ={
+        'product': product
+    }
+
+    return render(request,'products/product.html', context)
 
 def search(request):
     return render(request,'products/search.html')
